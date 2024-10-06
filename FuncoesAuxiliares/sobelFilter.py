2af925c284415391110579be3218ed871bef6c46
guirosasantos <guilherme.santos@allog.com.br>
2024-10-06 18:10:23 -0300

    wip: sobel e passa alta

---
+++ FuncoesAuxiliares/sobelFilter.py
@@ -0,0 +1,40 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+
+def ApplySobelFilter(image):
+    """
+    Aplica o filtro de Sobel em uma imagem.
+
+    Args:
+        image: A imagem de entrada.
+
+    Returns:
+        A imagem resultante da aplicação do filtro de Sobel.
+    """
+    sobelX = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
+    sobelY = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
+    sobel = np.sqrt(sobelX**2 + sobelY**2)
+    sobel = np.array(sobel, dtype=np.float32)
+    sobel -= sobel.min()
+    sobel = sobel*255/sobel.max()
+    return np.array(sobel, dtype=np.uint8)
+
+def createPA(shape, center, radius=35, lpType=2, n=2):
+    rows, cols = shape[:2]
+    r, c = np.mgrid[0:rows:1, 0:cols:1]
+    c -= center[0]
+    r -= center[1]
+    d = np.power(c, 2.0) + np.power(r, 2.0)
+    lpFilter_matrix = np.zeros(shape, np.float32)
+    if lpType == 0:  # Ideal high pass filter
+        lpFilter = np.copy(d)
+        lpFilter[lpFilter < pow(radius, 2.0)] = 0
+        lpFilter[lpFilter >= pow(radius, 2.0)] = 1
+    elif lpType == 1: #Butterworth Highpass Filters 
+        lpFilter = 1.0 - 1.0 / (1 + np.power(np.sqrt(d)/radius, 2*n))
+    elif lpType == 2: # Gaussian Highpass Filter 
+        lpFilter = 1.0 - np.exp(-d/(2*pow(radius, 2.0)))
+    lpFilter_matrix[:, :, 0] = lpFilter
+    lpFilter_matrix[:, :, 1] = lpFilter
+    return lpFilter_matrix
--- app.py
+++ app.py
@@ -65,20 +65,61 @@ print(f"Métricas da imagem com filtro passa baixa gaussiano: PSNR: {psnrGaussia
 # Obtendo resultados dos filtros
 
 sobelFilteredImage = sobelFilter.ApplySobelFilter(originalImage)
-idealPassHighFilteredImage = sobelFilter.ApplyPassHighFilter(originalImage)
-gaussianPassHighFilteredImage = sobelFilter.ApplyGaussianPassHighFilter(originalImage)
+
+idealPassHighFilteredImage = sobelFilter.createPA(convertedImage.shape, center=maxLoc)
+idealPassHighFilteredImage = convertedImage * idealPassHighFilteredImage
+idealPassHighFilteredImage = np.fft.ifftshift(idealPassHighFilteredImage)
+idealPassHighFilteredImage = cv2.idft(idealPassHighFilteredImage)
+idealPassHighFilteredImage = cv2.magnitude(idealPassHighFilteredImage[:, :, 0], idealPassHighFilteredImage[:, :, 1])
+idealPassHighFilteredImage = np.array(idealPassHighFilteredImage, dtype=np.float32)
+idealPassHighFilteredImage = np.abs(idealPassHighFilteredImage)
+idealPassHighFilteredImage -= idealPassHighFilteredImage.min()
+idealPassHighFilteredImage = idealPassHighFilteredImage*255/idealPassHighFilteredImage.max()
+idealPassHighFilteredImage = idealPassHighFilteredImage.astype(np.uint8)
+
+gaussianPassHighFilteredImage = sobelFilter.createPA(convertedImage.shape, center=maxLoc, lpType=2)
+gaussianPassHighFilteredImage = convertedImage * gaussianPassHighFilteredImage
+gaussianPassHighFilteredImage = np.fft.ifftshift(gaussianPassHighFilteredImage)
+gaussianPassHighFilteredImage = cv2.idft(gaussianPassHighFilteredImage)
+gaussianPassHighFilteredImage = cv2.magnitude(gaussianPassHighFilteredImage[:, :, 0], gaussianPassHighFilteredImage[:, :, 1])
+gaussianPassHighFilteredImage = np.array(gaussianPassHighFilteredImage, dtype=np.float32)
+gaussianPassHighFilteredImage = np.abs(gaussianPassHighFilteredImage)
+gaussianPassHighFilteredImage -= gaussianPassHighFilteredImage.min()
+gaussianPassHighFilteredImage = gaussianPassHighFilteredImage*255/gaussianPassHighFilteredImage.max()
+gaussianPassHighFilteredImage = gaussianPassHighFilteredImage.astype(np.uint8)
 
 # Mostrando os resultados
 
-sobelFilter.showSobelResults(sobelFilteredImage, idealPassHighFilteredImage)
-sobelFilter.showSobelResults(sobelFilteredImage, gaussianPassHighFilteredImage)
+gaussianFilter.showGaussianResults(sobelFilteredImage, idealPassHighFilteredImage, "Imagem com Filtro Passa Alta Ideal", "Imagem com Filtro de Sobel")
+gaussianFilter.showGaussianResults(sobelFilteredImage, gaussianPassHighFilteredImage, "Imagem com Filtro Passa Alta Gaussiano" , "Imagem com Filtro de Sobel")
 
 # Métricas
 
-psnrSobel, mseSobel, rmseSobel = metrics.CalculateMetrics(originalImage, sobelFilteredImage)
-psnrIdealPassHigh, mseIdealPassHigh, rmseIdealPassHigh = metrics.CalculateMetrics(originalImage, idealPassHighFilteredImage)
-psnrGaussianPassHigh, mseGaussianPassHigh, rmseGaussianPassHigh = metrics.CalculateMetrics(originalImage, gaussianPassHighFilteredImage)
+psnrIdealPassHigh, mseIdealPassHigh, rmseIdealPassHigh = metrics.CalculateMetrics(sobelFilteredImage, idealPassHighFilteredImage)
+psnrGaussianPassHigh, mseGaussianPassHigh, rmseGaussianPassHigh = metrics.CalculateMetrics(sobelFilteredImage, gaussianPassHighFilteredImage)
 
-print(f"Métricas da imagem com filtro de Sobel: PSNR: {psnrSobel}, MSE: {mseSobel}, RMSE: {rmseSobel}")
+print("\n")
 print(f"Métricas da imagem com filtro passa alta ideal: PSNR: {psnrIdealPassHigh}, MSE: {mseIdealPassHigh}, RMSE: {rmseIdealPassHigh}")
 print(f"Métricas da imagem com filtro passa alta gaussiano: PSNR: {psnrGaussianPassHigh}, MSE: {mseGaussianPassHigh}, RMSE: {rmseGaussianPassHigh}")
+
+#* Terceira parte: Comparação detecção de bordas
+
+# Obtendo resultados dos filtros
+
+cannyFilteredImage = cannyEdgeDetection.ApplyCannyEdgeDetection(originalImage)
+cannyGaussianFilteredImage = cannyEdgeDetection.ApplyCannyEdgeDetection(gaussianFilteredImage)
+cannyIdealPassLowFilteredImage = cannyEdgeDetection.ApplyCannyEdgeDetection(idealPassLowFilteredImage)
+cannyGaussianPassLowFilteredImage = cannyEdgeDetection.ApplyCannyEdgeDetection(gaussianPassLowFilteredImage)
+cannySobelFilteredImage = cannyEdgeDetection.ApplyCannyEdgeDetection(sobelFilteredImage)
+cannyIdealPassHighFilteredImage = cannyEdgeDetection.ApplyCannyEdgeDetection(idealPassHighFilteredImage)
+cannyGaussianPassHighFilteredImage = cannyEdgeDetection.ApplyCannyEdgeDetection(gaussianPassHighFilteredImage)
+
+# Mostrando os resultados
+
+gaussianFilter.showGaussianResults(cannyFilteredImage, cannyGaussianFilteredImage, "Imagem com Filtro de Canny", "Imagem com Filtro Gaussiano e Canny")
+gaussianFilter.showGaussianResults(cannyFilteredImage, cannyIdealPassLowFilteredImage, "Imagem com Filtro de Canny", "Imagem com Filtro Passa Baixa Ideal e Canny")
+gaussianFilter.showGaussianResults(cannyFilteredImage, cannyGaussianPassLowFilteredImage, "Imagem com Filtro de Canny", "Imagem com Filtro Passa Baixa Gaussiano e Canny")
+gaussianFilter.showGaussianResults(cannyFilteredImage, cannySobelFilteredImage, "Imagem com Filtro de Canny", "Imagem com Filtro de Sobel e Canny")
+gaussianFilter.showGaussianResults(cannyFilteredImage, cannyIdealPassHighFilteredImage, "Imagem com Filtro de Canny", "Imagem com Filtro Passa Alta Ideal e Canny")
+gaussianFilter.showGaussianResults(cannyFilteredImage, cannyGaussianPassHighFilteredImage, "Imagem com Filtro de Canny", "Imagem com Filtro Passa Alta Gaussiano e Canny")
+
