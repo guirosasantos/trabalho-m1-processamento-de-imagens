d4121b53cfa22d525b140160403101b9b95bc398
guirosasantos <guilherme.santos@allog.com.br>
2024-10-30 20:11:15 -0300

    rename projeto2

--- projeto2.py
+++ projeto2.py
@@ -4,15 +4,11 @@ from FuncoesAuxiliares import print, skinDetection, kmeans, seeding
 
 # Carregando as imagens
 
-fingerPrintImage = cv2.imread('Images/dedo.jpg', cv2.IMREAD_GRAYSCALE)
 faceImage = cv2.imread('Images/tiozao.jpg')
 secondFaceImage = cv2.imread('Images/tiozao2.png')
 
-print.ShowImage(fingerPrintImage, 'Finger Print')
-
 print.ShowImage(faceImage, 'Face')
-
-#* Primeira parte: – Limiarização Básica x Otsu:
+print.ShowImage(secondFaceImage, 'Second Face')
 
 #* Segunda parte: Detecção de rostos em imagens
 
